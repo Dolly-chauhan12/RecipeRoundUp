efabdd1c838fbbece4f5d3ba4d28bbd34147c44c
Dolly <abcddolly1234@gmail.com>
2023-11-24 21:08:31 +0530

    Added basic types

---
+++ src/types.d.ts
@@ -0,0 +1,56 @@
+// types for API response
+interface RecipeImage {
+  asset: {
+    url: string;
+  };
+}
+
+interface UserRefrence {
+  _ref: string;
+  _key: string;
+}
+
+export interface User {
+  _id: string;
+  userName: string;
+  image: string;
+}
+
+interface Comment {
+  comment: string;
+  _key: string;
+  postedBy: User;
+}
+
+export interface RecipePost {
+  _id: string;
+  image: RecipeImage;
+  ingredients?: string[];
+  recipe?: string;
+  title: string;
+  userId: string;
+  likes: UserRefrence[];
+  postedBy: User;
+}
+
+export interface RecipeDetail {
+  _id: string;
+  category: string;
+  image: RecipeImage;
+  ingredients?: string[];
+  recipe: string;
+  title: string;
+  userId: string;
+  likes: UserRefrence[];
+  postedBy: User;
+  comments: Comment[] | null;
+}
+
+export interface UserPost {
+  userId: string;
+  _id: string;
+  postedBy: User;
+  image: RecipeImage;
+  title: string;
+  likes: UserRefrence[];
+}
