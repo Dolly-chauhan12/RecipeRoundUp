c3e48fedc5494b2c867a2c2ad3d4a2da1e74948b
Dolly <abcddolly1234@gmail.com>
2024-02-04 19:37:23 +0530

    Added prev/next page buttons on feed

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import useQuery from "../utils/useQuery";
 import { client } from "../client";
 import {
@@ -9,12 +9,14 @@ import {
 } from "../utils/data";
 import { Spinner, RecipeCard, NoResult } from "./";
 import { RecipePost } from "../types";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 const Feed = () => {
   const [posts, setPosts] = useState<RecipePost[]>([]);
   const [sortBy, setSortBy] = useState<string>("dateCreated"); // Default sort option
 
   const [loading, setLoading] = useState<boolean>(false);
+  const [totalPosts, setTotalPosts] = useState<number>(0);
 
   let query: URLSearchParams = useQuery();
 
@@ -23,6 +25,11 @@ const Feed = () => {
   useEffect(() => {
     if (categoryId) {
       setLoading(true);
+      client
+        .fetch(
+          `count(*[_type == "post" && title match '${categoryId}*' || category match '${categoryId}*'])`
+        )
+        .then((data) => setTotalPosts(data));
       let queryTerm;
       if (sortBy === "likeCount") {
         queryTerm = searchQueryByLikes(categoryId);
@@ -35,7 +42,9 @@ const Feed = () => {
       });
     } else {
       setLoading(true);
-
+      client
+        .fetch(`count(*[_type == "post"])`)
+        .then((data) => setTotalPosts(data));
       let queryTerm;
       if (sortBy === "likeCount") {
         queryTerm = feedQueryByLikes;
@@ -96,6 +105,28 @@ const Feed = () => {
           {posts.map((post) => (
             <RecipeCard post={post} key={post._id} />
           ))}
+
+          {totalPosts > 10 ? (
+            <div className="w-full flex justify-between my-3 text-white">
+              <button
+                type="button"
+                className="flex items-center gap-2 text-xl rounded-md bg-green-900 px-3 py-2 font-semibold text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-900"
+              >
+                <FaAngleLeft />
+
+                <span>Prev</span>
+              </button>
+              <button
+                type="button"
+                className="flex items-center gap-2 text-xl rounded-md bg-green-900 px-3 py-2 font-semibold text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-900"
+              >
+                <span>Next</span>
+                <FaAngleRight />
+              </button>
+            </div>
+          ) : (
+            <> </>
+          )}
         </>
       ) : (
         <NoResult text={`Sorry , No Recipes Found`} />
