3f560092e5cd67828b01cc3b72b82d846a68f26f
Dolly <abcddolly1234@gmail.com>
2024-04-10 22:18:35 +0530

    recipe-card fixes

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -7,12 +7,28 @@ interface RecipeCardProps {
   post: RecipePost;
   searchPage?: boolean;
 }
+const colors = [
+  "bg-orange-100",
+  "bg-lime-100",
+  "bg-amber-100",
+  "bg-slate-100",
+  "bg-green-100",
+  "bg-indigo-100",
+  "bg-red-100",
+  "bg-cyan-100",
+  "bg-violet-100",
+  "bg-fuchsia-100",
+];
 
 const RecipeCard = ({ post, searchPage }: RecipeCardProps) => {
   const { postedBy, image, _id, title, recipe, likes } = post;
 
+  const random = Math.floor(Math.random() * colors.length);
+
+  const cardClass = `flex flex-col rounded-md  pb-3 transform duration-500 hover:-translate-y-2 hover:shadow-2xl ${colors[random]}`;
+
   return (
-    <div className="flex flex-col rounded-md bg-slate-100 pb-3 transform duration-500 hover:-translate-y-2 hover:shadow-2xl hover:bg-slate-200">
+    <div className={cardClass}>
       <div>
         <div className="flex gap-1.5 p-2 cursor-pointer font-semibold rounded ">
           <div className="md:w-14 md:h-12 w-10 h-10 pl-1">
@@ -40,32 +56,32 @@ const RecipeCard = ({ post, searchPage }: RecipeCardProps) => {
         </div>
       </div>
 
-      <div className="lg:ml-20 flex gap-4 relative ml-3">
-        <div>
-          <Link to={`/home/post-detail/${_id}`}>
-            <h2 className=" font-semibold text-2xl mb-1 line-clamp-1 px-2 font-hind">
+      <div className=" flex gap-4 relative ml-3">
+        <Link to={`/home/post-detail/${_id}`}>
+          <div className="group flex justify-center text-center relative overflow-hidden rounded-md cursor-pointer p-8">
+            <img
+              alt="recipe"
+              src={image.asset.url}
+              className="lg:w-[600px] lg:h-[372px] md:w-[550px] md:h-[400px]  sm:w-[86vw] sm:h-[56vh] w-[80vw] h-[40vh] object-cover rounded-2xl cursor-pointer bg-gray-100 ease-in-out duration-500  md:group-hover:scale-110 "
+            ></img>
+          </div>
+          <div>
+            <h2 className=" font-semibold text-2xl line-clamp-1 px-8 font-hind">
               {title}{" "}
             </h2>
-            <div className="group flex justify-center text-center relative overflow-hidden rounded-md cursor-pointer">
-              <img
-                alt="recipe"
-                src={image.asset.url}
-                className="lg:w-[600px] lg:h-[372px] md:w-[550px] md:h-[400px]  sm:w-[86vw] sm:h-[56vh] w-[80vw] h-[40vh] object-cover rounded-2xl cursor-pointer bg-gray-100 ease-in-out duration-500  md:group-hover:scale-125 "
-              ></img>
-            </div>
+          </div>
 
-            <p className="font-bold mt-1 px-2">
-              Liked by {likes?.length ? likes?.length : 0} people
-            </p>
-          </Link>
-        </div>
-        {recipe && recipe.length > 0 && !searchPage && (
+          <p className="font-bold mt-1 px-8">
+            Liked by {likes?.length ? likes?.length : 0} people
+          </p>
+        </Link>
+        {/* {recipe && recipe.length > 0 && !searchPage && (
           <div className="w-1/2 mt-2 pt-5  h-[390px]  hidden md:block ml-1">
             <p className="line-clamp-15  font-medium whitespace-pre-wrap font-lexend">
               {recipe}
             </p>
           </div>
-        )}
+        )} */}
       </div>
     </div>
   );
