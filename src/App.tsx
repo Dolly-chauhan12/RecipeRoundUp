d0c59c1c7ec40fcce91efab2f2eb00df11905eef
Dolly <abcddolly1234@gmail.com>
2024-04-12 20:21:40 +0530

    setup ui library

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,25 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import ScrollToTop from "./utils/ScrollToTop";
 import Home from "./containers/Home";
 import { About } from "./components";
+import { NextUIProvider } from "@nextui-org/react";
 
 function App() {
   return (
-    <div>
-      <GoogleOAuthProvider
-        clientId={`${process.env.REACT_APP_PUBLIC_GOOGLE_API_TOKEN}`}
-      >
-        <ScrollToTop />
+    <NextUIProvider>
+      <div>
+        <GoogleOAuthProvider
+          clientId={`${process.env.REACT_APP_PUBLIC_GOOGLE_API_TOKEN}`}
+        >
+          <ScrollToTop />
 
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/home/*" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </GoogleOAuthProvider>
-    </div>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/home/*" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </GoogleOAuthProvider>
+      </div>
+    </NextUIProvider>
   );
 }
 
